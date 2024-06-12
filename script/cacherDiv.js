bc2719ff4cf2b795116dc1a078998aa0feff878a
Theo <brissettheo@gmail.com>
2024-06-12 21:39:33 +0200

    suppression fichier js inutilisés

--- script/cacherDiv.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    if(localStorage.getItem('trajets') !== null){
-        const cacherDiv = document.getElementById('cacher');
-        cacherDiv.classList.add('hidden');
-        cacherDiv.classList.remove('cacher');
-    
-    }
-})
